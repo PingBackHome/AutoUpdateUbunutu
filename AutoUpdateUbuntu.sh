f124ad36b7cb1483fd4bb937cf2f7936078428f0
PingBackHome <115549820+PingBackHome@users.noreply.github.com>
2023-02-27 13:14:13 +0100

    Update AutoUpdateUbuntu.sh

--- AutoUpdateUbuntu.sh
+++ AutoUpdateUbuntu.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-if [ $(date +\%u) -eq 3 ] && [ $(date +\%d) -le 07 ]; then
-  LOG_FILE="$HOME/update.log"
-  sudo apt update | sed 's/, /\'$'\n/g' > $LOG_FILE
-  sudo apt upgrade -y | sed 's/, /\'$'\n/g' >> $LOG_FILE
-fi
+
+# Set variables for header and log file
+HEADER="IT2Grow v1.0.0 - Update Log"
+LOG_FILE="$(date +%Y-%m-%d)-update-log.txt"
+
+# Output header to console
+echo "$HEADER"
+echo "Date of Update: $(date)"
+
+# Update packages
+sudo apt update >> "$LOG_FILE" 2>&1
+
+# Upgrade packages
+sudo apt upgrade -y >> "$LOG_FILE" 2>&1
+
+# Remove unnecessary packages
+sudo apt autoremove -y >> "$LOG_FILE" 2>&1
+
+# Clean up
+sudo apt clean >> "$LOG_FILE" 2>&1
+
+# Format log file by replacing commas with newlines
+sed -i 's/,/\n/g' "$LOG_FILE"
+
+# Add header to log file
+echo -e "$HEADER\nDate of Update: $(date)\n" | cat - "$LOG_FILE" > temp && mv temp "$LOG_FILE"
+
+# Export log file to home folder of the user
+cp "$LOG_FILE" ~/
+
+# Output message to console
+echo "Update complete. Log file exported to home folder."
