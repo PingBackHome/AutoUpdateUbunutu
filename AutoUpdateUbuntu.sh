cc9a4b8459b76d3de6c12e43517b858a0227bd96
PingBackHome <115549820+PingBackHome@users.noreply.github.com>
2023-02-27 12:46:56 +0100

    Update AutoUpdateUbuntu.sh

--- AutoUpdateUbuntu.sh
+++ AutoUpdateUbuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ $(date +\%u) -eq 3 ] && [ $(date +\%d) -le 07 ]; then
-  sudo apt update && sudo apt upgrade -y
+  LOG_FILE="$HOME/update.log"
+  sudo apt update | sed 's/, /\'$'\n/g' > $LOG_FILE
+  sudo apt upgrade -y | sed 's/, /\'$'\n/g' >> $LOG_FILE
 fi
-
